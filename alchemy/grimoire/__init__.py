73b7e8cbdf944f03837881d80fe4df26f017b839
Magdalena Kalder <mkalder@1-E-27.42heilbronn.de>
2026-08-22 15:13:50 +0200

    done

--- alchemy/grimoire/__init__.py
+++ alchemy/grimoire/__init__.py
@@ -1,2 +1,1 @@
 from .light_spellbook import light_spell_record
-from .dark_spellbook import dark_spell_record
--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -1,22 +1,31 @@
-try:
-    import alchemy.grimoire
-except ImportError:
-        print("hallo")
-        
-
-
 def main():
     print("=== Kaboom 1 ===")
     print("Access to alchemy/grimoire/dark_spellbook.py directly")
     print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
+    import alchemy.grimoire.dark_spellbook
+
     spell_name: str = "Dark magic"
     ingredients: str = "Bats and frogs"
 
     print(
             f"Testing record dark spell:"
-            f" {alchemy.grimoire.dark_spell_record(spell_name, ingredients)}"
+            f" {alchemy.grimoire.dark_spellbook.dark_spell_record(spell_name, ingredients)}"
             )
 
 
 if __name__ == "__main__":
     main()
+
+    # try:
+    #     import alchemy.grimoire.dark_spellbook
+
+    #     spell_name: str = "Dark magic"
+    #     ingredients: str = "Bats and frogs"
+
+    #     print(
+    #             f"Testing record dark spell:"
+    #             f" {alchemy.grimoire.dark_spellbook.dark_spell_record"
+    #              f"(spell_name, ingredients)}"
+    #             )
+    # except ImportError:
+    #     print("ImportError - circular import!")
